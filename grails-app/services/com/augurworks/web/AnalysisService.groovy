cceb1286413e5843b3b03804f42cbf9d5a819b60
TheConnMan <bcconn2112@gmail.com>
2014-02-16 17:48:20 -0500

    Committing AnalysisService.

--- grails-app/services/com/augurworks/web/AnalysisService.groovy
+++ grails-app/services/com/augurworks/web/AnalysisService.groovy
@@ -5,7 +5,18 @@ import grails.transaction.Transactional
 @Transactional
 class AnalysisService {
 	
+	DataController dataController = new DataController();
 	
+	def linearRegression(parameters) {
+		def inputData = dataController.getData(parameters);
+		
+	}
+	
+	def decisionTree() {
+		def data = dataController.getData(parameters);
+		
+		
+	}
 
     def serviceMethod() {
 
