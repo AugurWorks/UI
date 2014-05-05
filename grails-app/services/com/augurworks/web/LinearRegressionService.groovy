ebaad77cbb6da5cd6b4c2cb7540750c50d884c3d
Stephen Freiberg <safreiberg@gmail.com>
2014-05-04 23:08:12 -0700

    Normalize linear regression inputs

--- grails-app/services/com/augurworks/web/LinearRegressionService.groovy
+++ grails-app/services/com/augurworks/web/LinearRegressionService.groovy
@@ -15,34 +15,32 @@ class LinearRegressionService {
     def dataService
 
     def performAnalysis(parameters, inputData, removed) {
-		if (parameters.analysis.dependent in removed) {
-			return ['root' : [:], 'metadata' : ['errors' : ['Invalid ticker' : 'The dependent variable is an invalid ticker.']]]
-		}
-		parameters.analysis.independent.split(', ').each { val ->
-			if (val in removed) {
-				parameters.analysis.independent = (parameters.analysis.independent.split(', ') - val).join(', ')
-			}
-		}
+        if (parameters.analysis.dependent in removed) {
+            return ['root' : [:], 'metadata' : ['errors' : ['Invalid ticker' : 'The dependent variable is an invalid ticker.']]]
+        }
+        parameters.analysis.independent.split(', ').each { val ->
+            if (val in removed) {
+                parameters.analysis.independent = (parameters.analysis.independent.split(', ') - val).join(', ')
+            }
+        }
         def js = inputData as JSON
         def builder = new groovy.json.JsonBuilder()
         def temp = [];
-		temp.push(parameters.analysis);
-		js.target.each { k, v -> println v.dates.size() }
+        temp.push(parameters.analysis);
+        js.target.each { k, v -> println v.dates.size() }
         def root = builder.root {
             analysis temp
             data js.target
         }
         DataTransferObject dataObject = DataTransferObjects.fromJsonString(builder.toString());
         LinRegAnalysisParam param = dataObject.getAnalysis().get(AnalysisParamType.LINREG);
-        //println "data object: " + dataObject
-        //println "param: " + param
+
         double[] dependent = getDependentFromData(dataObject, param);
         double[][] independent = getIndependentFromData(dataObject, param);
         def result = new JsonSlurper().parseText(
             LinearRegressions.getJsonString(
                 LinearRegressions.getLinearRegression(independent, dependent)));
 
-        //println "result: " + result
         def names = parameters.analysis.independent.split(', ')
         def coeff = result.parameter_estimates.split(',').collect { it.toDouble() }
         def keys = [:]
@@ -60,8 +58,8 @@ class LinearRegressionService {
                 log.error 'performAnalysis: ' + e
             }
         }
-		def tempObj = inputData[keys[parameters.analysis.dependent]].metadata.req as JSON
-		tempObj = JSON.parse(tempObj.toString())
+        def tempObj = inputData[keys[parameters.analysis.dependent]].metadata.req as JSON
+        tempObj = JSON.parse(tempObj.toString())
         inputData['-1'] = ['dates': tempData, 'metadata': ['valid': true, 'errors': [:], unit: inputData[keys[parameters.analysis.dependent]].metadata.unit,'name': 'LinReg', req: tempObj]]
         inputData['-1'].metadata.req.name = inputData['-1'].metadata.req.name + ' - LineReg'
         return ['root': inputData, 'metadata': result]
@@ -89,8 +87,6 @@ class LinearRegressionService {
             }
             dateCounter++;
         }
-        //println "Number of dates: " + values.length
-        //println "Number of independent variables: " + values[0].length
         return values;
     }
 }
--- src/java/com/augurworks/stats/LinearRegressions.java
+++ src/java/com/augurworks/stats/LinearRegressions.java
@@ -5,15 +5,19 @@ import java.util.List;
 import org.apache.commons.math3.stat.regression.MillerUpdatingRegression;
 import org.apache.commons.math3.stat.regression.RegressionResults;
 import org.apache.commons.math3.stat.regression.UpdatingMultipleLinearRegression;
+import org.apache.log4j.Logger;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import com.google.gson.JsonObject;
 
 public class LinearRegressions {
+    private static final Logger log = Logger.getLogger(LinearRegressions.class);
 
     public static String getJsonString(UpdatingMultipleLinearRegression reg) {
+        log.info("Translating regression to JSON...");
         RegressionResults regress = reg.regress();
+        log.info("Regression results completed.");
         JsonObject jsonObj = new JsonObject();
         jsonObj.addProperty("adjusted_r_squared", regress.getAdjustedRSquared());
         jsonObj.addProperty("r_squared", regress.getRSquared());
@@ -21,6 +25,7 @@ public class LinearRegressions {
         jsonObj.addProperty("parameter_estimates", Joiner.on(",").join(params));
         List<Double> errs = getList(regress.getStdErrorOfEstimates());
         jsonObj.addProperty("parameter_std_error", Joiner.on(",").join(errs));
+        log.info("Regression converted to JSON.");
         return jsonObj.toString();
     }
 
@@ -33,13 +38,40 @@ public class LinearRegressions {
     }
 
     public static UpdatingMultipleLinearRegression getLinearRegression(double[][] x, double[] y) {
+        log.info("Getting linear regression...");
         validateDataLengths(x, y);
+        normalizeValues(x, y);
         MillerUpdatingRegression regression = new MillerUpdatingRegression(x[0].length, true);
         regression.addObservations(x, y);
+        log.info("Regression created.");
         return regression;
     }
 
+    private static void normalizeValues(double[][] x, double[] y) {
+        log.info("Normalizing values...");
+        normalizeValues(y);
+        for (int loc = 0; loc < x.length; loc++) {
+            normalizeValues(x[loc]);
+        }
+        log.info("Values normalized.");
+    }
+
+    private static void normalizeValues(double[] ds) {
+        if (ds[0] == 0) {
+            log.info("Initial value cannot be zero. Setting it to 0.01.");
+            ds[0] = 0.01;
+        }
+        double initial = ds[0];
+        for (int i = 0; i < ds.length; i++) {
+            // percent increase = (current - starting) / starting
+            ds[i] = (ds[i] - initial) / initial;
+        }
+    }
+
     private static void validateDataLengths(double[][] x, double[] y) {
+        if (x.length < 1) {
+            throw new IllegalArgumentException("X must have at least one independent variable.");
+        }
         if (x.length != y.length) {
             throw new IllegalArgumentException("X and Y should have the same length. Instead, "
                     + "X had length " + x.length + " and Y had length " + y.length);
