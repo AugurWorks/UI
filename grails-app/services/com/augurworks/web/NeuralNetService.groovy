cff4f6be699cbbb26e600a5fbef48fa1ead5726d
Brian Conn <bcconn2112@gmail.com>
2014-07-27 16:19:12 -0400

    Added test augsave file. Added augtrain serializer function.

--- grails-app/services/com/augurworks/web/NeuralNetService.groovy
+++ grails-app/services/com/augurworks/web/NeuralNetService.groovy
@@ -5,9 +5,28 @@ import grails.transaction.Transactional
 
 @Transactional
 class NeuralNetService {
+	
+	def grailsApplication
 
     def performAnalysis(parameters, inputData, removed) {
-		println parameters.analysis
+		writeFile(parameters.analysis, inputData)
 		return [success: true]
     }
+	
+	def writeFile(param, data) {
+		def name = 'Test' + new Date().format('MM-dd-yy-HH-mm-ss-SSS') + '.augtrain';
+		def f = grailsApplication.mainContext.getResource('neuralnet/' + name).file;
+		f << 'net ' + data.size() + ',' + param.depth + '\n'
+		f << 'train 1,' + param.rounds + ',' + param.learningConstant + ',' + param.rounds + ',' + param.cutoff + '\n'
+		f << 'TITLES ' + data.values().collect { it.metadata.req.name }.join(',') + '\n'
+		def dates = data.values().collect { it.dates.values() };
+		(0..(dates[0].size() - 1)).each { i ->
+			f << dates[0][i] + ' ' + dates[1..(dates.size() - 1)].collect { it[i] }.join(',') + '\n'
+		}
+		f.close()
+	}
+	
+	def readResult(name) {
+		def f = grailsApplication.mainContext.getResource('neuralnet/' + name).file;
+	}
 }
