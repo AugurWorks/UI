1d4422c2b4d600ecfcaf9ee70ddce8d31d105825
Stephen Freiberg <safreiberg@gmail.com>
2014-07-27 21:23:42 -0700

    Finish up augtrain format writing

--- grails-app/services/com/augurworks/web/NeuralNetService.groovy
+++ grails-app/services/com/augurworks/web/NeuralNetService.groovy
@@ -5,28 +5,33 @@ import grails.transaction.Transactional
 
 @Transactional
 class NeuralNetService {
-	
-	def grailsApplication
+
+    def grailsApplication
 
     def performAnalysis(parameters, inputData, removed) {
-		writeFile(parameters.analysis, inputData)
-		return [success: true]
+        writeFile(parameters.analysis, inputData)
+        return [success: true]
     }
-	
-	def writeFile(param, data) {
-		def name = 'Test' + new Date().format('MM-dd-yy-HH-mm-ss-SSS') + '.augtrain';
-		def f = grailsApplication.mainContext.getResource('neuralnet/' + name).file;
-		f << 'net ' + data.size() + ',' + param.depth + '\n'
-		f << 'train 1,' + param.rounds + ',' + param.learningConstant + ',' + param.rounds + ',' + param.cutoff + '\n'
-		f << 'TITLES ' + data.values().collect { it.metadata.req.name }.join(',') + '\n'
-		def dates = data.values().collect { it.dates.values() };
-		(0..(dates[0].size() - 1)).each { i ->
-			f << dates[0][i] + ' ' + dates[1..(dates.size() - 1)].collect { it[i] }.join(',') + '\n'
-		}
-		f.close()
-	}
-	
-	def readResult(name) {
-		def f = grailsApplication.mainContext.getResource('neuralnet/' + name).file;
-	}
+
+    def writeFile(param, data) {
+        def name = 'Test' + new Date().format('MM-dd-yy-HH-mm-ss-SSS') + '.augtrain';
+        def f = grailsApplication.mainContext.getResource('neuralnet/' + name).file;
+        f << 'net ' + (data.size() - 1) + ',' + param.depth + '\n'
+        f << 'train 1,' + param.rounds + ',' + param.learningConstant + ',' + param.rounds + ',' + param.cutoff + '\n'
+        def titles = data.values().collect { it.metadata.req.name }.join(',')
+        titles = titles.substring(titles.indexOf(",") + 1)
+        f << 'TITLES ' + titles + '\n'
+        def dates = data.values().collect { it.dates.values() };
+        (0..(dates[0].size() - 1)).each { i ->
+            def values = dates[0][i] + ' ' + dates[1..(dates.size() - 1)].collect { it[i] }.join(',') + '\n'
+            if (!values.contains("null")) {
+                f << values
+            }
+        }
+        f.close()
+    }
+
+    def readResult(name) {
+        def f = grailsApplication.mainContext.getResource('neuralnet/' + name).file;
+    }
 }
